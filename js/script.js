799a222d541f3cef0e9c996f897ea9ffdc9dde09
Maciej Krzyżowski <maciek.krzyzowski44@gmail.com>
2020-06-08 19:21:26 +0200

    add  edit, change, delete task

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ let alertInfo;
 let btnAdd;
 let list;
 let newTask;
+let popup;
+let popupInfo;
+let editedTodo;
+let popupInput;
+let btnAccept;
+let btnCancle;
+let id = 0;
 
 const main = () => {
     prepareDOMElements();
@@ -14,16 +21,28 @@ const prepareDOMElements = () => {
     alertInfo = document.querySelector('.todo__alertInfo');
     btnAdd = document.querySelector('.btn__add');
     list = document.querySelector('.todo__list');
+    popup = document.querySelector('.popup');
+    popupInfo = document.querySelector('.popup__info');
+    popupInput = document.querySelector('.popup__input');
+    btnAccept = document.querySelector('.btn__accept');
+    btnCancle = document.querySelector('.btn__cancel');
 };
 
 const prepareDomEvents = () => {
     btnAdd.addEventListener('click', addNewTask);
+    input.addEventListener('keyup', enterCheck);
     list.addEventListener('click', checkClick);
-}
+    btnCancle.addEventListener('click', closePopup);
+    window.addEventListener('keyup', escCheckTask);
+    btnAccept.addEventListener('click', changeTask);
+    popupInput.addEventListener('keyup', enterChangeTask);
+};
 
 const addNewTask = () => {
     if (input.value !== '') {
+        id++;
         newTask = document.createElement('li');
+        newTask.setAttribute('id', id)
         newTask.classList.add('todo__element');
         newTask.innerText = input.value;
         list.appendChild(newTask);
@@ -33,8 +52,15 @@ const addNewTask = () => {
         alertInfo.innerText = '';
     } else {
         alertInfo.innerText = 'Wpisz treść zadania!';
-    }
-}
+    };
+};
+
+const enterCheck = e => {
+    if (e.keyCode === 13) {
+        addNewTask();
+    };
+};
+
 
 const createToolsArea = () => {
     let toolsPanel = document.createElement('div');
@@ -55,6 +81,66 @@ const createToolsArea = () => {
     toolsPanel.appendChild(btnEdit);
     toolsPanel.appendChild(btnDelete);
     newTask.appendChild(toolsPanel);
+};
+
+const checkClick = e => {
+    if (e.target.closest('button').classList.contains('btn__complete')) {
+        doneTask(e);
+    } else if (e.target.closest('button').classList.contains('btn__edit')) {
+        editTask(e);
+    } else if (e.target.closest('button').classList.contains('btn__delete')) {
+        deleteTask(e);
+    };
+};
+
+const doneTask = e => {
+    e.target.closest('li').classList.toggle('completed');
+    e.target.closest('button').classList.toggle('completed');
 }
+
+const editTask = e => {
+    const oldTodo = e.target.closest('li').id;
+    editedTodo = document.getElementById(oldTodo);
+    popupInput.value = editedTodo.firstChild.textContent;
+
+    popup.style.display = 'flex';
+    popupInput.focus();
+};
+
+const deleteTask = e => {
+    e.target.closest('li').remove();
+
+    if (list.children.length === 0) {
+        alertInfo.innerText = 'Brak zadań na liście.';
+    } else {
+        alertInfo.innerText = '';
+    }
+};
+
+const changeTask = () => {
+    if (popupInput.value !== '') {
+        editedTodo.firstChild.textContent = popupInput.value;
+        popup.style.display = 'none';
+        popupInfo.innerText = '';
+    } else {
+        popupInfo.innerText = 'Musisz podać treść zadania!';
+    };
+};
+
+const enterChangeTask = e => {
+    if (e.keyCode === 13) {
+        changeTask();
+    };
+};
+
+const closePopup = () => {
+    popup.style.display = 'none';
+};
+
+const escCheckTask = e => {
+    if (e.keyCode === 27) {
+        closePopup();
+    };
+};
 
 document.addEventListener('DOMContentLoaded', main);
